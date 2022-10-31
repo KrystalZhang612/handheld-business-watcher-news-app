7ac53cf7e6a0e8163ec6a7d8e4b5707b2e4ce69c
KrystalZhang612 <krystalzhang612@gmail.com>
2022-10-31 14:04:01 -0700

    Initial Commit

---
+++ Handheld-Business-Watcher-News-App/APICaller.swift
@@ -0,0 +1,76 @@
+//
+//  APICaller.swift
+//  Handheld-Business-Watcher-News-App
+//
+//  Created by Krystal Zhang on 10/31/22.
+//
+
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let topHeadlinesURL = URL(string:
+            "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=94c3ce509e0a4c8dad6544a7f2397726")
+    }
+    private init(){}
+    
+    public func getTopStories(completion: @escaping (Result<[Article], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error{
+                completion(.failure(error))
+            }
+            else if let data = data {
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    print("Articles: \(result.articles.count)")
+                    completion(.success(result.articles))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
+}
+
+//Models
+
+struct APIResponse: Codable {
+    let articles: [Article]
+    
+}
+
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+}
+
+struct Source: Codable {
+    
+    let name: String
+    
+}
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+
---
+++ Handheld-Business-Watcher-News-App/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Handheld-Business-Watcher-News-App
+//
+//  Created by Krystal Zhang on 10/31/22.
+//
+
+import UIKit
+import SafariServices
+
+//Table View
+//Custom Cell
+//API Caller
+//Open the News Story
+//Search for News Stories
+
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
+        return table
+        
+    }()
+    
+    private var articles = [Article]()
+    private var viewModels = [NewsTableViewCellViewModel]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Handheld Bussiness Watcher News App"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.backgroundColor = .systemBackground
+        
+        fetchTopStories()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func fetchTopStories(){
+        APICaller.shared.getTopStories{ [weak self] result in
+            switch result {
+            case .success(let articles):
+                self?.articles = articles
+                self?.viewModels = articles.compactMap({
+                    NewsTableViewCellViewModel(
+                        title: $0.title,
+                        subtitle: $0.description ?? "No Description",
+                        imageURL: URL(string: $0.urlToImage ?? "")
+                    )
+                })
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    //  Table
+    func tableView (_ tableView: UITableView, numberOfRowsInSection section: Int)-> Int {
+        return viewModels.count
+    }
+    
+    func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: NewsTableViewCell.identifier,
+            for: indexPath
+        ) as? NewsTableViewCell else {
+            fatalError()
+        }
+        cell.configure(with: viewModels[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let article = articles[indexPath.row]
+        
+        guard let url = URL(string: article.url ?? "") else {
+            return
+        }
+        
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+
+}
+
